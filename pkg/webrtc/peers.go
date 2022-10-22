71d2c4b0e593173ea7e79e8e13cb5adf01bd2cef
Aaron Watkins <ac.watkins80@gmail.com>
2022-10-22 04:58:53 +0000

    WIP bare bones peers.go

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -8,3 +8,41 @@ type Peers struct {
 	Connections []PerConnectionState
 	TrackLocals map[string]*webrtc.TrackLocalStaticRTP
 }
+
+type PeerConnectionState struct {
+	PeerConnection *webrtc.PeerConnection
+	websocket      *ThreadSafeWriter
+}
+
+type ThreadSafeWriter struct {
+	Conn  *websocket.Conn
+	Mutex sync.Mutex
+}
+
+func (t *ThreadSafeWriter) WriteJSON (v interface()) error {
+	t.Mutex.Lock()
+	defer t.Mutex.Unloack(
+		return t.Conn.WriteJSON(v)
+	)
+}
+
+func (p *Peers) AddTrack (t *webrts.TrackRemote) *webrtc.TrackLocalStaticRTP {
+
+}
+
+func (p *Peers) RemoveTrack (t *webrtc.TrackLocalStaticRTP) {
+
+}
+
+func (p *Peers) SignalPeerConnection() {
+
+}
+
+func (p *Peers) DispatchKeyFrame() {
+
+}
+
+type websocketMessage struct {
+	Event string `json:"event"`
+	Data string `json:"data"`
+}
